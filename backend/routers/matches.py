26f2a20fd8b1735ae274707f5f24cc32eea5ca14
Arvind <arvind152402@gmail.com>
2026-09-21 18:48:22 +0530

    Fix ObjectId JSON serialization crash for matches

--- backend/routers/matches.py
+++ backend/routers/matches.py
@@ -24,22 +24,28 @@ async def get_my_matches(user=Depends(get_current_user)):
         
         if lost_item:
             lost_item["_id"] = str(lost_item["_id"])
+            if "reportedBy" in lost_item: lost_item["reportedBy"] = str(lost_item["reportedBy"])
             doc["lostItem"] = lost_item
         else: doc["lostItem"] = None
             
         if found_item:
             found_item["_id"] = str(found_item["_id"])
+            if "reportedBy" in found_item: found_item["reportedBy"] = str(found_item["reportedBy"])
             doc["foundItem"] = found_item
         else: doc["foundItem"] = None
+            
+        is_approved = doc.get("status") in ["APPROVED", "COMPLETED"]
             
         if lost_user:
             lost_user["_id"] = str(lost_user["_id"])
             doc["lostUser"] = {"_id": lost_user["_id"], "name": lost_user.get("name"), "profileImage": lost_user.get("profileImage")}
+            if is_approved: doc["lostUser"]["phoneNumber"] = lost_user.get("phoneNumber")
         else: doc["lostUser"] = None
             
         if found_user:
             found_user["_id"] = str(found_user["_id"])
             doc["foundUser"] = {"_id": found_user["_id"], "name": found_user.get("name"), "profileImage": found_user.get("profileImage")}
+            if is_approved: doc["foundUser"]["phoneNumber"] = found_user.get("phoneNumber")
         else: doc["foundUser"] = None
 
         if doc.get("requestedBy"): doc["requestedBy"] = str(doc["requestedBy"])
@@ -61,11 +67,13 @@ async def get_match(id: str, user=Depends(get_current_user)):
     
     if lost_item:
         lost_item["_id"] = str(lost_item["_id"])
+        if "reportedBy" in lost_item: lost_item["reportedBy"] = str(lost_item["reportedBy"])
         match["lostItem"] = lost_item
     else: match["lostItem"] = None
         
     if found_item:
         found_item["_id"] = str(found_item["_id"])
+        if "reportedBy" in found_item: found_item["reportedBy"] = str(found_item["reportedBy"])
         match["foundItem"] = found_item
     else: match["foundItem"] = None
         
